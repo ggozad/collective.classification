f5212ddc69418d2c057e306afb098f73a0eb039e
Yiorgis Gozadinos <ggozad@qiweb.net>
2010-01-28 12:05:17 +0200

    fixed broken installation due to non-availability of nltk_data

--- qi/kb/classification/__init__.py
+++ qi/kb/classification/__init__.py
@@ -1,5 +1,7 @@
 from zope.i18nmessageid import MessageFactory
+from qi.kb.classification.nltkutilities.downloader import downloadNLTKData
 ClassificationMessageFactory = MessageFactory('qi.kb.classification')
+downloadNLTKData()
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
