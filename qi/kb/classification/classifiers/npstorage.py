7b4940bff46ede6aba925f54cc5ca788d61f6f07
Yiorgis Gozadinos <ggozad@qiweb.net>
2010-02-09 11:32:50 +0200

    removed obsolete fields

--- qi/kb/classification/classifiers/npstorage.py
+++ qi/kb/classification/classifiers/npstorage.py
@@ -2,9 +2,7 @@ from operator import itemgetter
 from zope.interface import implements
 from persistent import Persistent
 from persistent.mapping import PersistentMapping
-from BTrees.OOBTree import OOSet, union
 from nltk.metrics import ranks_from_scores
-from plone.memoize import instance
 from qi.kb.classification.classifiers.npextractor import NPExtractor
 from qi.kb.classification.interfaces import INounPhraseStorage
 
@@ -14,14 +12,12 @@ class NounPhraseStorage(Persistent):
     
     implements(INounPhraseStorage)
     
-    def __init__(self,tagger=None,noNounRanksToKeep = 10):
+    def __init__(self,tagger=None):
         """
         """
-        self.noNounRanksToKeep = noNounRanksToKeep
         self.rankedNouns = PersistentMapping()
         self.rankedNPs = PersistentMapping()
         self.extractor = NPExtractor(tagger=tagger)
-        self.allNouns = OOSet()
 
     def _scoresToRanks(self,rankdict):
         scored_items = sorted(rankdict.items(),key=itemgetter(1),reverse=True) 
@@ -37,14 +33,7 @@ class NounPhraseStorage(Persistent):
         (noun_scores,noun_phrase_scores) = self.extractor.extract(text)
         if noun_scores:
             ranked_nouns = self._scoresToRanks(noun_scores)
-            self.rankedNouns[doc_id] = ranked_nouns
-            
-            importantNouns = []
-            for (noun,score) in ranked_nouns:
-                if score < self.noNounRanksToKeep:
-                    importantNouns.append(noun)
-            self.allNouns = union(self.allNouns,OOSet(importantNouns))
-            
+            self.rankedNouns[doc_id] = ranked_nouns            
         if noun_phrase_scores:
             ranked_nps = self._scoresToRanks(noun_phrase_scores)
             self.rankedNPs[doc_id] = ranked_nps
